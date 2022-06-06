39ce157315f973a7ff4eaa46c14f1cd9a50ce43a
AlimMamutov <mamutov.creation@yandex.ru>
2022-06-07 01:14:27 +0300

    task 1

--- HW example/task_1/main.py
+++ HW example/task_1/main.py
@@ -7,7 +7,7 @@ info_3.txt и формирующий новый «отчетный» файл в
 
 Создать функцию get_data(), в которой в цикле осуществляется перебор файлов
 с данными, их открытие и считывание данных. В этой функции из считанных данных
-необходимо с помощью регулярных выражений или другого инструмента извлечь значения параметров
+необходимо с помощью регулярных выражений извлечь значения параметров
 «Изготовитель системы», «Название ОС», «Код продукта», «Тип системы».
 Значения каждого параметра поместить в соответствующий список. Должно
 получиться четыре списка — например, os_prod_list, os_name_list,
@@ -36,42 +36,65 @@ os_code_list, os_type_list. В этой же функции создать гл�
 
 ПРОШУ ВАС НЕ УДАЛЯТЬ СЛУЖЕБНЫЕ ФАЙЛЫ TXT И ИТОГОВЫЙ ФАЙЛ CSV!!!
 """
-import os, re
+
+import re
+import csv
 
 
-# в цикле осуществляется перебор файлов
-# с данными, их открытие и считывание данных. В этой функции из считанных данных
-# необходимо с помощью регулярных выражений или другого инструмента извлечь значения параметров
-# «Изготовитель системы», «Название ОС», «Код продукта», «Тип системы».
+def get_data():
+    """Извлечение данных из txt"""
 
-def regex(headers, data):
-    re_mask = r'{}'.format(header + ':\s*(.*)')
-    re_string = re.compile(re_mask)
-    elem = re_string.findall(data)
-    return elem
-
-def get_data(path='.'):
     os_prod_list = []
     os_name_list = []
     os_code_list = []
     os_type_list = []
     main_data = []
-    headers = ['Изготовитель системы', 'Название ОС', 'Код продукта', ' Тип системы']
-    reg_mask = r'Изготовитель системы:\s*(?P<word1>(.*))|Название ОС:\s*(?P<word2>(.*))'
 
-    # reg_mask = ''
-    # for header in headers:
-    #     reg_mask += header + ':\s*(.*)|'
-    # reg_mask = r'{}'.format(reg_mask)[:-1]
-    files = list(filter(lambda x: re.match(r'info_\d*.txt',x), os.listdir(path)))
-    for file in files:
-        with open(file, 'r', encoding='utf-8') as data:
-            # re_string = re.compile(reg_mask)
-            re_str = re.compile(r'Название ОС:\s*(?P<word1>.*)|Изготовитель системы:\s*(?P<word2>.*)')
-            elem = re_str.findall(data.read())
-            # dd = elem.group('word2')
-            a = 0
+    for i in range(1, 4):
+        file_obj = open(f'info_{i}.txt')
+        data = file_obj.read()
+
+        # Получаем список изготовителей системы
+        os_prod_reg = re.compile(r'Изготовитель системы:\s*\S*')
+        os_prod_list.append(os_prod_reg.findall(data)[0].split()[2])
+
+        # Название ОС
+        os_name_reg = re.compile(r'Windows\s\S*')
+        os_name_list.append(os_name_reg.findall(data)[0])
+
+        # Код продукта
+        os_code_reg = re.compile(r'Код продукта:\s*\S*')
+        os_code_list.append(os_code_reg.findall(data)[0].split()[2])
+
+        # Тип системы
+        os_type_reg = re.compile(r'Тип системы:\s*\S*')
+        os_type_list.append(os_type_reg.findall(data)[0].split()[2])
+
+    headers = ['Изготовитель системы', 'Название ОС', 'Код продукта', 'Тип системы']
+    main_data.append(headers)
+
+    j = 1
+    for i in range(0, 3):
+        row_data = []
+        row_data.append(j)
+        row_data.append(os_prod_list[i])
+        row_data.append(os_name_list[i])
+        row_data.append(os_code_list[i])
+        row_data.append(os_type_list[i])
+        main_data.append(row_data)
+        j += 1
+    return main_data
 
 
+def write_to_csv(out_file):
+    """Запись данных в csv"""
 
+    main_data = get_data()
+    with open(out_file, 'w', encoding='utf-8') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
+        for row in main_data:
+            writer.writerow(row)
+
+
+# write_to_csv('data_report.csv')
 get_data()
